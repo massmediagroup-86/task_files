e778219b3fbfbd67be2d1a26701a554b764e20ba
Maksym Bobikov <mmg.bobikov.m@gmail.com>
2019-11-08 16:56:19 +0200

    init commit

---
+++ app/Http/Controllers/ReportController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\UserFile;
+use Illuminate\Http\Request;
+
+class ReportController extends Controller
+{
+
+    /**
+     * @param UserFile $file
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function show(UserFile $file)
+    {
+        return view('reports.show',['file'=>$file]);
+    }
+}
